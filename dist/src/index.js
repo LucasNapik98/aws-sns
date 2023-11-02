56c58f7e602c2de366fe62718814ad6e90f45efe
Lucas Napik <lucas.napik@outlook.com>
2023-11-02 14:08:18 +0100

    build

---
+++ dist/src/index.js
@@ -0,0 +1,43 @@
+"use strict";
+var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@actions/core");
+const client_sns_1 = require("@aws-sdk/client-sns");
+const credential_providers_1 = require("@aws-sdk/credential-providers");
+console.log(process.env);
+const region = (0, core_1.getInput)("region", {
+    required: true,
+    trimWhitespace: true,
+});
+const topic = (0, core_1.getInput)("topic", {
+    trimWhitespace: true,
+});
+const subject = (0, core_1.getInput)("subject", {
+    required: false,
+    trimWhitespace: true,
+});
+const message = (0, core_1.getInput)("message", {
+    required: true,
+});
+const client = new client_sns_1.SNSClient({
+    credentials: (0, credential_providers_1.fromEnv)(),
+    region,
+});
+const structure = (_a = (0, core_1.getInput)("structure", {
+    required: true,
+})) !== null && _a !== void 0 ? _a : "json";
+publishMessage(topic, message, structure, subject)
+    .then((response) => {
+    (0, core_1.setOutput)("message_id", response.MessageId);
+})
+    .catch((e) => {
+    (0, core_1.setFailed)(e);
+});
+function publishMessage(topic, message, structure, subject) {
+    return client.send(new client_sns_1.PublishCommand({
+        TopicArn: topic,
+        Subject: subject,
+        Message: message,
+        MessageStructure: structure,
+    }));
+}
