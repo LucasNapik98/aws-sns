e8c91669e029154ce690e5ab7c28480aaa54adcc
Lucas Napik <lucas.napik@outlook.com>
2023-11-02 14:15:58 +0100

    test

--- dist/src/index.js
+++ dist/src/index.js
@@ -2,7 +2,8 @@
 var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@actions/core");
-console.log(process.env);
+const client_sns_1 = require("@aws-sdk/client-sns");
+const credential_providers_1 = require("@aws-sdk/credential-providers");
 const region = (0, core_1.getInput)("region", {
     required: true,
     trimWhitespace: true,
@@ -17,32 +18,25 @@ const subject = (0, core_1.getInput)("subject", {
 const message = (0, core_1.getInput)("message", {
     required: true,
 });
-// const client = new SNSClient({
-//   credentials: fromEnv(),
-//   region,
-// });
+const client = new client_sns_1.SNSClient({
+    credentials: (0, credential_providers_1.fromEnv)(),
+    region,
+});
 const structure = (_a = (0, core_1.getInput)("structure", {
     required: true,
 })) !== null && _a !== void 0 ? _a : "json";
-// publishMessage(topic, message, structure, subject)
-//   .then((response) => {
-//     setOutput("message_id", response.MessageId);
-//   })
-//   .catch((e) => {
-//     setFailed(e);
-//   });
-// function publishMessage(
-//   topic: string,
-//   message: string,
-//   structure: string,
-//   subject?: string
-// ): Promise<PublishCommandOutput> {
-//   return client.send(
-//     new PublishCommand({
-//       TopicArn: topic,
-//       Subject: subject,
-//       Message: message,
-//       MessageStructure: structure,
-//     })
-//   );
-// }
+publishMessage(topic, message, structure, subject)
+    .then((response) => {
+    (0, core_1.setOutput)("message_id", response.MessageId);
+})
+    .catch((e) => {
+    (0, core_1.setFailed)(e);
+});
+function publishMessage(topic, message, structure, subject) {
+    return client.send(new client_sns_1.PublishCommand({
+        TopicArn: topic,
+        Subject: subject,
+        Message: message,
+        MessageStructure: structure,
+    }));
+}
