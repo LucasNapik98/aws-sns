4c40ec8147c810bca64038829e9c0d8a21d6c382
Lucas Napik <lucas.napik@outlook.com>
2023-11-02 14:11:07 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -36,26 +36,26 @@ const structure =
     required: true,
   }) ?? "json";
 
-publishMessage(topic, message, structure, subject)
-  .then((response) => {
-    setOutput("message_id", response.MessageId);
-  })
-  .catch((e) => {
-    setFailed(e);
-  });
+// publishMessage(topic, message, structure, subject)
+//   .then((response) => {
+//     setOutput("message_id", response.MessageId);
+//   })
+//   .catch((e) => {
+//     setFailed(e);
+//   });
 
-function publishMessage(
-  topic: string,
-  message: string,
-  structure: string,
-  subject?: string
-): Promise<PublishCommandOutput> {
-  return client.send(
-    new PublishCommand({
-      TopicArn: topic,
-      Subject: subject,
-      Message: message,
-      MessageStructure: structure,
-    })
-  );
-}
+// function publishMessage(
+//   topic: string,
+//   message: string,
+//   structure: string,
+//   subject?: string
+// ): Promise<PublishCommandOutput> {
+//   return client.send(
+//     new PublishCommand({
+//       TopicArn: topic,
+//       Subject: subject,
+//       Message: message,
+//       MessageStructure: structure,
+//     })
+//   );
+// }
