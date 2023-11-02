541f51fcf9d3083ddf456b12bf514bf84098d3cc
Lucas Napik <lucas.napik@outlook.com>
2023-11-02 14:14:51 +0100

    #0

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,6 @@ import {
 } from "@aws-sdk/client-sns";
 import { fromEnv } from "@aws-sdk/credential-providers";
 
-console.log(process.env);
-
 const region = getInput("region", {
   required: true,
   trimWhitespace: true,
@@ -26,36 +24,36 @@ const message = getInput("message", {
   required: true,
 });
 
-// const client = new SNSClient({
-//   credentials: fromEnv(),
-//   region,
-// });
+const client = new SNSClient({
+  credentials: fromEnv(),
+  region,
+});
 
 const structure =
   getInput("structure", {
     required: true,
   }) ?? "json";
 
-// publishMessage(topic, message, structure, subject)
-//   .then((response) => {
-//     setOutput("message_id", response.MessageId);
-//   })
-//   .catch((e) => {
-//     setFailed(e);
-//   });
+publishMessage(topic, message, structure, subject)
+  .then((response) => {
+    setOutput("message_id", response.MessageId);
+  })
+  .catch((e) => {
+    setFailed(e);
+  });
 
-// function publishMessage(
-//   topic: string,
-//   message: string,
-//   structure: string,
-//   subject?: string
-// ): Promise<PublishCommandOutput> {
-//   return client.send(
-//     new PublishCommand({
-//       TopicArn: topic,
-//       Subject: subject,
-//       Message: message,
-//       MessageStructure: structure,
-//     })
-//   );
-// }
+function publishMessage(
+  topic: string,
+  message: string,
+  structure: string,
+  subject?: string
+): Promise<PublishCommandOutput> {
+  return client.send(
+    new PublishCommand({
+      TopicArn: topic,
+      Subject: subject,
+      Message: message,
+      MessageStructure: structure,
+    })
+  );
+}
